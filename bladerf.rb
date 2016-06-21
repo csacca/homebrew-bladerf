211d8c8af4ea369580617fa815daafcbd55b3768
Christopher Sacca <csacca@Christophers-MacBook-Pro-5.local>
2016-06-20 22:57:08 -0400

    Cleaned up script and added some args and requirements

--- bladerf.rb
+++ bladerf.rb
@@ -6,13 +6,21 @@ class Bladerf < Formula
 
   depends_on 'cmake' => :build
   depends_on 'libusb'
+  depends_on 'pkgconfig'
+  depends_on 'libtecla'
 
   def install
     cd 'host'
     mkdir 'build' do
-      system 'cmake', '..', *std_cmake_args << "-DINSTALL_UDEV_RULES=OFF"
-      system 'make'
-      system 'make install'
+      args = %W[
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=#{prefix}
+        -DCMAKE_C_COMPILER=#{ENV.cc}
+        -DINSTALL_UDEV_RULES=OFF
+      ]
+      system "cmake", "..", *args, *std_cmake_args
+      system "make"
+      system "make install"
     end
   end
 end
